80e153442a1b2145915ef6e9660bb77cc42396b8
Matias Leandro <matiasleandroheredia@gmail.com>
2026-03-05 21:56:27 +0000

    feat(network): add bulk file processing and refactor architecture

--- ip_tracker.py
+++ ip_tracker.py
@@ -3,6 +3,7 @@ import urllib.request
 import json
 import sys
 import ipaddress
+import argparse
 
 def is_valid_public_ip(ip_input):
     # Verify valid and public ip input
@@ -28,19 +29,18 @@ def get_ip_info(ip_adress=""):
     try:
         response = urllib.request.urlopen(url)
         raw_data = response.read().decode('utf-8')
-        json_data = json.loads(raw_data)
-
-        return json_data
+        return json.loads(raw_data)
 
     except Exception as e:
         print(f"[!] Connection error: {e}")
         return None
 
 def print_info(ip_info):
-    query_response = ip_info['query'] 
-    country_response = ip_info['country']
-    city_response = ip_info['city']
-    isp_response = ip_info['isp']
+    query_response = ip_info.get('query', 'N/A')
+    country_response = ip_info.get('country', 'Unknown')
+    city_response = ip_info.get('city', 'Unknown')
+    isp_response = ip_info.get('isp', 'Unknown')
+
     print(f"""
 [+] Target IP: {query_response}
 [+] Country: {country_response}
@@ -48,15 +48,50 @@ def print_info(ip_info):
 [+] ISP: {isp_response}
 """)
 
-if __name__ == "__main__":
-    result = ""
-    if len(sys.argv) > 1:
-        if not is_valid_public_ip(sys.argv[1]): 
-            sys.exit(1)
-        result = get_ip_info(sys.argv[1])
-    else:
-        result = get_ip_info()
+def execute_ip_lookup(ip_target=""):
+    result = get_ip_info(ip_target)
     if result is not None and result.get('status') == 'success':
         print_info(result)
     else:
-        print("[!] Empty Information")
+        print(f"[!] Empty Information for IP: {ip_target if ip_target else 'Local'}")
+
+def process_file(file_path):
+    print(f"[*] Reading IP file: {file_path}")
+    try:
+        with open(file_path, 'r') as file:
+
+            for line in file:
+                target_ip = line.strip()
+
+                if not target_ip:
+                    continue
+
+                print(f"\n--- Researching: {target_ip} ---")
+
+                if is_valid_public_ip(target_ip):
+                    execute_ip_lookup(target_ip)
+
+    except FileNotFoundError:
+        print(f"[!] Error: The file was not found: '{file_path}'")
+
+if __name__ == "__main__":
+    # Parser Definition
+    parser = argparse.ArgumentParser(description="IP Tracker OSINT Tool")
+    parser.add_argument('-i', '--input_ip', type=str, help='Specific input IP')
+    parser.add_argument('-f', '--file', type=str, help='IP file to process')
+
+    # Read user args
+    args = parser.parse_args()
+
+    # Routing
+    if args.file:
+        process_file(args.file)
+
+    elif args.input_ip:
+        if not is_valid_public_ip(args.input_ip):
+            sys.exit(1)
+
+        execute_ip_lookup(args.input_ip)
+
+    else:
+        execute_ip_lookup()
